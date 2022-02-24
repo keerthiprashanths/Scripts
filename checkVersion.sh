13c8b505af0d4c78ca3eb3717427ac56bbab1bfb
keerthiprashanths <35592906+keerthiprashanths@users.noreply.github.com>
2022-02-24 08:13:21 +0530

    Update checkVersion.sh

--- checkVersion.sh
+++ checkVersion.sh
@@ -1,26 +1,56 @@
-#!/bin/bash
-#my_dir="$1"
-printf "Checking for file .bashrc"
-list=`cat ~/.bashrc | grep 'export PATH' | tail -1 | grep -P /home/.*?goldenversions/.*?/ -o | cut -d ":" -f 1`
-printf "\n Here is list of tools installed "
-#echo "$list"
-json_var="["
-json_var=$json_var' \n "Tools" :'
-for i in $list
-do
-	json_var=$json_var'{ \n'
-	product_name=`echo $i | cut -d "/" -f 5`
-	json_var=$json_var'"product" : "'$product_name'", \n'
-	version=`echo $product_name | cut -d "-" -f 2`
-	api_response=`curl https://reqbin.com/echo/get/json   2> /dev/null`
-	printf "\n Version $product_name of is  $version"
-	if [ "$api_response" = "{\"success\":\"tru2e\"}" ] ; then
-		printf "\n Latest version is installed no changes req for $product_name"
-	else
-		printf "\n Calling cleanup script... for $product_name"
-		op=`sh cleanup.sh`
-		printf "\n Installing latest version"
-	fi
-	
-	printf "Response $api_response "
-done
+#!/usr/bin/env bash
+PRODUCT=jruby
+VERSION=	
+SUBPROID=$2
+JSONFILE="$HOME/.script/JsonFile"
+OS=$3
+JQPRODUCT="jq-linux"
+JQVERSION="1.6"
+#JQ integration
+
+if type -p unzip; then
+   echo "unzip avaialble"
+elif [ $OS == rh ]  then
+    then
+   sudo  yum install unzip -y
+else
+    sudo apt install unzip
+fi
+
+if [ -d "$JQPRODUCT" ]
+then
+  echo "jq is present"
+  export  jq=$HOME/.script/jq-linux/jq
+
+ else
+   wget -P $HOME/.script https://artifactory.global.standardchartered.com/artifactory/technology-standard-release/application/business-application/corporate/commercial-and-institutional-banking/jq/$JQVERSION/$JQPRODUCT-$JQVERSION.zip
+   unzip $HOME/.script/$JQPRODUCT-$JQVERSION.zip
+   export  jq=$HOME/.script/jq-linux/jq
+   rm -rf $HOME/.script/$JQPRODUCT-$JQVERSION.zip
+   
+fi
+
+if [ -f "$JSONFILE" ]
+then
+     OLD_VERSION=($($jq -r '.'$PRODUCT'.version' $JSONFILE))
+     INSTALPATH=($($jq -r '.'$PRODUCT'."'$OLD_VERSION'".locationOfInstallation' $JSONFILE))
+ else
+echo "JsonFile Not Present"
+fi
+
+
+if [ -z "$INSTALPATH" ] || [ $INSTALPATH == 'null' ]
+then
+  INSTALPATH=`cat ~/.bashrc | grep 'export PATH' | tail -1 | grep -P /.*?/.*?goldenversions/.*?/ -o | egrep -i "$PRODUCT" | cut -d ":" -f 1`
+  #OLD_VERSION="echo $INSTALPATH |cut -d "-" -f 4"
+   echo "$INSTALPATH"
+ else
+   echo "old product installed in $INSTALPATH"
+fi
+[ -z "$INSTALPATH" ] && echo "there is no  older product installed in system"
+
+
+echo $OLD_VERSION
+echo $INSTALPATH
+
+#sh $HOME/.script/cleanup.sh $PRODUCT $OLD_VERSION $INSTALPATH
